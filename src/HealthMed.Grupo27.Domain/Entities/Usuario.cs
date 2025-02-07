7b1170576fe522d33caefb30601249a903759e5e
Tiago Henrique de Souza <tiago.hsouza7@gmail.com>
2025-02-06 22:17:09 -0300

    Alteração Entidade Usuário.

--- src/HealthMed.Grupo27.Domain/Entities/Usuario.cs
+++ src/HealthMed.Grupo27.Domain/Entities/Usuario.cs
@@ -10,16 +10,10 @@ namespace HealthMed.Grupo27.Domain.Entities
     {
         public int IdUsuario { get; set; }
 
-        public string Nome { get; set; } 
-
-        public string Email { get; set; }
-
-        public string CPF { get; set; }
-
-        public string Telefone { get; set; }
-
         public string Senha { get; set; }
 
-        public DateTime DataNascimento { get; set; }
+        public List<Medico> Medicos { get; set; }
+
+        public List<Paciente> Pacientes { get; set; }
     }
 }
