2821ee16a674535568c44f4048a3e3387a6169d0
Tiago Henrique de Souza <tiago.hsouza7@gmail.com>
2025-02-08 22:15:15 -0300

    Correções nos endpoints.

---
+++ src/HealthMed.Grupo27.Infrastructure/Migrations/20250209003743_HealthMed07.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HealthMed.Grupo27.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class HealthMed07 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropCheckConstraint("PK_HorariosMedicos_1","HorariosMedicos");
+            migrationBuilder.DropColumn("IdHorario", "HorariosMedicos");
+
+            migrationBuilder.AddColumn<int>(
+                name: "IdHorario",
+                table: "HorariosMedicos",
+                type: "int",
+                nullable: false,
+                defaultValue: 0
+                ).Annotation("SqlServer:Identity", "1, 1");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+            name: "IdMedico",
+            table: "Medicos",
+            nullable: false)
+            .OldAnnotation("SqlServer:Identity", "1, 1");
+        }
+    }
+}
--- src/HealthMed.Grupo27.Infrastructure/Repositories/ConsultaRepository.cs
+++ src/HealthMed.Grupo27.Infrastructure/Repositories/ConsultaRepository.cs
@@ -23,7 +23,8 @@ namespace HealthMed.Grupo27.Infrastructure.Repositories
         {
             return await _context.Consultas.AnyAsync(c => c.IdMedico == idMedico &&
                                                           ((inicio >= c.HoraInicio && inicio < c.HoraFim) ||
-                                                          (fim > c.HoraInicio && fim <= c.HoraFim)));
+                                                          (fim > c.HoraInicio && fim <= c.HoraFim))
+                                                          && c.StatusConsulta != Domain.Enums.StatusConsultaType.Cancelada);
         }
 
         public async Task AdicionarAsync(Consulta consulta)
@@ -53,9 +54,9 @@ namespace HealthMed.Grupo27.Infrastructure.Repositories
             throw new NotImplementedException();
         }
 
-        public Task<Consulta> ObterPorIdAsync(int idConsulta)
+        public async Task<Consulta> ObterPorIdAsync(int idConsulta)
         {
-            throw new NotImplementedException();
+            return await _context.Consultas.FirstOrDefaultAsync(m => m.IdConsulta == idConsulta);
         }
 
         Task<Consulta> IConsultaRepository.GetConsultasAsync()
